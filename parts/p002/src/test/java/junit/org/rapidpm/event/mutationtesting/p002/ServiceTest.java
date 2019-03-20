0ce94fc91656c27c42ecb9996c142f2ce7f71f90
Sven Ruppert <sven.ruppert@gmail.com>
2019-03-20 08:58:30 +0200

    init

---
+++ parts/p002/src/test/java/junit/org/rapidpm/event/mutationtesting/p002/ServiceTest.java
@@ -0,0 +1,19 @@
+package junit.org.rapidpm.event.mutationtesting.p002;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Create the best test coverage you can for this
+ */
+class ServiceTest {
+
+  @Test
+  void test001() {
+    //start here...
+  }
+
+  @Test
+  void test002() {
+
+  }
+}
