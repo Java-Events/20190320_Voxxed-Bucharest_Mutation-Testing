504bc291fc9b0f763b01efb92d10d13b08adae24
Sven Ruppert <sven.ruppert@gmail.com>
2019-03-20 12:42:57 +0200

    add p011

--- parts/p001/src/test/java/junit/org/rapidpm/event/mutationtesting/p001/MyArroundClassExtension.java
+++ parts/p001/src/test/java/junit/org/rapidpm/event/mutationtesting/p001/MyArroundClassExtension.java
@@ -5,8 +5,13 @@ import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ExtensionContext.Namespace;
 
-public class MyArroundClassExtension implements BeforeAllCallback, AfterAllCallback {
-	private Namespace namespace = Namespace.create("AroundClassNamespace");
+public class MyArroundClassExtension
+		implements BeforeAllCallback,
+		AfterAllCallback {
+
+
+	private Namespace namespace
+			= Namespace.create("AroundClassNamespace");
 
 	@Override
 	public void beforeAll(ExtensionContext context) throws Exception {
--- parts/p002/src/test/java/junit/org/rapidpm/event/mutationtesting/p002/ServiceTest.java
+++ parts/p002/src/test/java/junit/org/rapidpm/event/mutationtesting/p002/ServiceTest.java
@@ -1,6 +1,7 @@
 package junit.org.rapidpm.event.mutationtesting.p002;
 
 import org.junit.jupiter.api.Test;
+import org.rapidpm.event.mutationtesting.p002.Service;
 
 /**
  * Create the best test coverage you can for this
@@ -10,10 +11,14 @@ class ServiceTest {
   @Test
   void test001() {
     //start here...
+    final Service service = new Service();
+
+
+
   }
 
   @Test
   void test002() {
-
+    final Service service = new Service();
   }
 }
